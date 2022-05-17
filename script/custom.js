6cb576f3c0300f979292a50317fc640789dc72e0
gyuhwa9922 <100964041+gyuhwa9922@users.noreply.github.com>
2022-05-18 00:07:06 +0900

    Create custom.js
    d

---
+++ script/custom.js
@@ -0,0 +1,7 @@
+$('p').css({'display':'none'});
+$('.show_btn').click(function(){
+    $('p').css({'display':'block'});
+});
+$('.hide_btn').click(function(){
+    $('p').css({'display':'none'});
+});
